6fd7701a7b99f9d3ef32377584d81326bbb5295c
jsquie <squires.jr@gmail.com>
2024-05-16 19:36:17 -0700

    refactor to functional paradigm for adaa

--- src/editor.rs
+++ src/editor.rs
@@ -1,59 +1,131 @@
-use nih_plug::prelude::Editor;
+use crate::AtomicF32;
+use nih_plug::prelude::{util, Editor};
 use nih_plug_vizia::vizia::prelude::*;
 use nih_plug_vizia::widgets::*;
 use nih_plug_vizia::{assets, create_vizia_editor, ViziaState, ViziaTheming};
+use std::sync::atomic::Ordering;
 use std::sync::Arc;
 
 use crate::NonlinearAdaaParams;
 
+const STYLE: &str = r#"
+    .foo {
+        width: 300px; 
+    }
+    
+"#;
+
 #[derive(Lens)]
 struct Data {
     params: Arc<NonlinearAdaaParams>,
+    input_peak_meters: [Arc<AtomicF32>; 2],
+    output_peak_meters: [Arc<AtomicF32>; 2],
 }
 
 impl Model for Data {}
 
 // Makes sense to also define this here, makes it a bit easier to keep track of
 pub(crate) fn default_state() -> Arc<ViziaState> {
-    ViziaState::new(|| (500, 500))
+    ViziaState::new(|| (500, 600))
 }
 
 pub(crate) fn create(
     params: Arc<NonlinearAdaaParams>,
+    input_meters: [Arc<AtomicF32>; 2],
+    output_meters: [Arc<AtomicF32>; 2],
     editor_state: Arc<ViziaState>,
 ) -> Option<Box<dyn Editor>> {
     create_vizia_editor(editor_state, ViziaTheming::Custom, move |cx, _| {
         assets::register_noto_sans_light(cx);
         assets::register_noto_sans_thin(cx);
+        assets::register_noto_sans_bold(cx);
+
+        let _ = cx.add_stylesheet(STYLE);
 
         Data {
             params: params.clone(),
+            input_peak_meters: input_meters.clone(),
+            output_peak_meters: output_meters.clone(),
         }
         .build(cx);
 
         VStack::new(cx, |cx| {
-            Label::new(cx, "Nonlinear ADAA")
-                .font_size(30.0)
-                .height(Pixels(50.0));
+            HStack::new(cx, |cx| {
+                VStack::new(cx, |_| {});
+                VStack::new(cx, |cx| {
+                    Label::new(cx, "Nonlinear ADAA")
+                        .font_size(30.0)
+                        .height(Pixels(50.0))
+                        .width(Pixels(300.0))
+                        .text_align(TextAlign::Center);
+                })
+                .class("foo");
+                VStack::new(cx, |_| {});
+            })
+            .height(Pixels(100.0));
 
-            Label::new(cx, "Gain").top(Pixels(10.0));
-            ParamSlider::new(cx, Data::params, |params| &params.gain);
+            HStack::new(cx, |cx| {
+                VStack::new(cx, |cx| {
+                    Label::new(cx, "Gain").top(Pixels(10.0));
+                    ParamSlider::new(cx, Data::params, |params| &params.gain);
 
-            Label::new(cx, "Output");
-            ParamSlider::new(cx, Data::params, |params| &params.output);
+                    Label::new(cx, "Output");
+                    ParamSlider::new(cx, Data::params, |params| &params.output);
 
-            Label::new(cx, "Prefilter Cutoff Frequency");
-            ParamSlider::new(cx, Data::params, |params| &params.pre_filter_cutoff);
+                    Label::new(cx, "Prefilter Cutoff Frequency");
+                    ParamSlider::new(cx, Data::params, |params| &params.pre_filter_cutoff);
 
-            Label::new(cx, "Clip Style");
-            ParamSlider::new(cx, Data::params, |params| &params.nl_proc_type);
+                    Label::new(cx, "Clip Style");
+                    ParamSlider::new(cx, Data::params, |params| &params.nl_proc_type);
 
-            Label::new(cx, "ADAA order");
-            ParamSlider::new(cx, Data::params, |params| &params.nl_proc_order);
+                    Label::new(cx, "ADAA order");
+                    ParamSlider::new(cx, Data::params, |params| &params.nl_proc_order);
 
-            Label::new(cx, "Oversampling");
-            ParamSlider::new(cx, Data::params, |params| &params.os_level);
-        })
-        .top(Pixels(10.0));
+                    Label::new(cx, "Oversampling");
+                    ParamSlider::new(cx, Data::params, |params| &params.os_level);
+                })
+                .border_width(Pixels(5.0))
+                .border_color(Color::black())
+                .left(Pixels(5.0));
+
+                VStack::new(cx, |cx| {
+                    Label::new(cx, "Input Level").top(Pixels(10.0));
+                    PeakMeter::new(
+                        cx,
+                        Data::input_peak_meters.map(|peak_meter| {
+                            util::gain_to_db(peak_meter[0].load(Ordering::Relaxed))
+                        }),
+                        Some(Duration::from_millis(600)),
+                    );
+                    PeakMeter::new(
+                        cx,
+                        Data::input_peak_meters.map(|peak_meter| {
+                            util::gain_to_db(peak_meter[1].load(Ordering::Relaxed))
+                        }),
+                        Some(Duration::from_millis(600)),
+                    )
+                    .bottom(Pixels(5.0));
+
+                    Label::new(cx, "Output Level");
+                    PeakMeter::new(
+                        cx,
+                        Data::output_peak_meters.map(|peak_meter| {
+                            util::gain_to_db(peak_meter[0].load(Ordering::Relaxed))
+                        }),
+                        Some(Duration::from_millis(600)),
+                    );
+                    PeakMeter::new(
+                        cx,
+                        Data::output_peak_meters.map(|peak_meter| {
+                            util::gain_to_db(peak_meter[1].load(Ordering::Relaxed))
+                        }),
+                        Some(Duration::from_millis(600)),
+                    );
+                })
+                .right(Pixels(5.0))
+                .border_width(Pixels(5.0))
+                .border_color(Color::blue());
+            });
+        });
     })
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,40 @@
-use adaa_nl::{ADAAFirst, ADAASecond, NLProc, NextAdaa};
+use adaa_nl::{
+    AntiderivativeOrder, NonlinearProcessor, ProcessorState, ProcessorState::State, ProcessorStyle,
+};
 use iir_biquad_filter::{FilterOrder, IIRBiquadFilter};
+use itertools::izip;
 use nih_plug::prelude::*;
 use nih_plug_vizia::ViziaState;
 use oversampler::{Oversample, OversampleFactor};
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::atomic::Ordering;
 use std::sync::Arc;
 
 mod editor;
 
 const MAX_BLOCK_SIZE: usize = 32;
 const MAX_OS_FACTOR_SCALE: usize = 16;
-
-#[derive(Enum, Debug, PartialEq)]
-pub enum AntiderivativeOrder {
-    #[id = "first order ad"]
-    #[name = "First Order"]
-    First,
-
-    #[id = "second order ad"]
-    #[name = "Second Order"]
-    Second,
-}
+const PEAK_METER_DECAY_MS: f64 = 150.0;
 
 pub struct NonlinearAdaa {
     params: Arc<NonlinearAdaaParams>,
-    first_order_nlprocs: Vec<ADAAFirst>,
-    second_order_nlprocs: Vec<ADAASecond>,
-    proc_order: AntiderivativeOrder,
-    oversamplers: Vec<Oversample>,
+    non_linear_processors: [NonlinearProcessor; 2],
+    proc_state: ProcessorState,
+    oversamplers: [Oversample; 2],
     over_sample_process_buf: [f32; MAX_BLOCK_SIZE * MAX_OS_FACTOR_SCALE],
     pre_filters: [IIRBiquadFilter; 2],
+    peak_meter_decay_weight: f32,
+    input_meters: [Arc<AtomicF32>; 2],
+    output_meters: [Arc<AtomicF32>; 2],
 }
 
 #[derive(Params, Debug)]
 pub struct NonlinearAdaaParams {
-    /// The parameter's ID is used to identify the parameter in the wrappred plugin API. As long as
-    /// these IDs remain constant, you can rename and reorder these fields as you wish. The
-    /// parameters are exposed to the host in the same order they were defined. In this case, this
-    /// gain parameter is stored as linear gain while the values are displayed in decibels.
     #[id = "gain"]
     pub gain: FloatParam,
     #[id = "output"]
     pub output: FloatParam,
     #[id = "nl proc"]
-    pub nl_proc_type: EnumParam<NLProc>,
+    pub nl_proc_type: EnumParam<ProcessorStyle>,
     #[id = "ad level"]
     pub nl_proc_order: EnumParam<AntiderivativeOrder>,
     #[id = "os level"]
@@ -57,38 +48,44 @@ pub struct NonlinearAdaaParams {
 impl Default for NonlinearAdaa {
     fn default() -> Self {
         Self {
-            params: Arc::new(NonlinearAdaaParams::default()),
-            first_order_nlprocs: Vec::with_capacity(2),
-            second_order_nlprocs: Vec::with_capacity(2),
-            proc_order: AntiderivativeOrder::First,
-            oversamplers: Vec::with_capacity(2),
+            params: Arc::new(NonlinearAdaaParams::new()),
+            non_linear_processors: [NonlinearProcessor::new(), NonlinearProcessor::new()],
+            proc_state: State(ProcessorStyle::HardClip, AntiderivativeOrder::FirstOrder),
+            oversamplers: [
+                Oversample::new(OversampleFactor::TwoTimes, MAX_BLOCK_SIZE),
+                Oversample::new(OversampleFactor::TwoTimes, MAX_BLOCK_SIZE),
+            ],
             over_sample_process_buf: [0.0; MAX_OS_FACTOR_SCALE * MAX_BLOCK_SIZE],
             pre_filters: [IIRBiquadFilter::default(), IIRBiquadFilter::default()],
+            peak_meter_decay_weight: 1.0,
+            input_meters: [
+                Arc::new(AtomicF32::new(util::MINUS_INFINITY_DB)),
+                Arc::new(AtomicF32::new(util::MINUS_INFINITY_DB)),
+            ],
+            output_meters: [
+                Arc::new(AtomicF32::new(util::MINUS_INFINITY_DB)),
+                Arc::new(AtomicF32::new(util::MINUS_INFINITY_DB)),
+            ],
         }
     }
 }
 
-impl Default for NonlinearAdaaParams {
-    fn default() -> Self {
+impl NonlinearAdaaParams {
+    fn new() -> Self {
         let oversampling_times = Arc::new(AtomicF32::new(oversampling_factor_to_times(
             OversampleFactor::TwoTimes,
         )));
 
         Self {
-            // This gain is stored as linear gain. NIH-plug comes with useful conversion functions
-            // to treat these kinds of parameters as if we were dealing with decibels. Storing this
-            // as decibels is easier to work with, but requires a conversion for every sample.
             editor_state: editor::default_state(),
 
             gain: FloatParam::new(
                 "Gain",
                 util::db_to_gain(0.0),
                 FloatRange::Skewed {
-                    min: util::db_to_gain(-30.0),
-                    max: util::db_to_gain(30.0),
-                    // This makes the range appear as if it was linear when displaying the values as
-                    // decibels
-                    factor: FloatRange::gain_skew_factor(-30.0, 30.0),
+                    min: util::db_to_gain(0.0),
+                    max: util::db_to_gain(60.0),
+                    factor: FloatRange::gain_skew_factor(0.0, 60.0),
                 },
             )
             .with_smoother(SmoothingStyle::OversamplingAware(
@@ -118,9 +115,9 @@ impl Default for NonlinearAdaaParams {
                 &SmoothingStyle::Logarithmic(5.0),
             )),
 
-            nl_proc_type: EnumParam::new("Nonlinear Process", NLProc::HardClip),
+            nl_proc_type: EnumParam::new("Nonlinear Process", ProcessorStyle::HardClip),
 
-            nl_proc_order: EnumParam::new("Antiderivative Order", AntiderivativeOrder::First),
+            nl_proc_order: EnumParam::new("Antiderivative Order", AntiderivativeOrder::FirstOrder),
 
             os_level: EnumParam::new("Oversample Factor", OversampleFactor::TwoTimes)
                 .with_callback(Arc::new(move |new_factor| {
@@ -199,7 +196,8 @@ impl Plugin for NonlinearAdaa {
     fn editor(&mut self, _async_executor: AsyncExecutor<Self>) -> Option<Box<dyn Editor>> {
         editor::create(
             self.params.clone(),
-            // self.peak_meter.clone(),
+            self.input_meters.clone(),
+            self.output_meters.clone(),
             self.params.editor_state.clone(),
         )
     }
@@ -210,21 +208,10 @@ impl Plugin for NonlinearAdaa {
         buffer_config: &BufferConfig,
         _context: &mut impl InitContext<Self>,
     ) -> bool {
-        let num_channels = _audio_io_layout
+        let _num_channels = _audio_io_layout
             .main_output_channels
             .expect("Plugin was initialized without any outputs")
             .get() as usize;
-
-        self.first_order_nlprocs.resize_with(num_channels, || {
-            ADAAFirst::new(self.params.nl_proc_type.value())
-        });
-        self.second_order_nlprocs.resize_with(num_channels, || {
-            ADAASecond::new(self.params.nl_proc_type.value())
-        });
-
-        self.oversamplers.resize_with(num_channels, || {
-            Oversample::new(self.params.os_level.value(), MAX_BLOCK_SIZE)
-        });
 
         self.oversamplers
             .iter_mut()
@@ -242,21 +229,35 @@ impl Plugin for NonlinearAdaa {
             FilterOrder::First,
         );
 
-        // Resize buffers and perform other potentially expensive initialization operations here.
-        // The `reset()` function is always called right after this function. You can remove this
-        // function if you do not need it.
+        let new_state = State(
+            self.params.nl_proc_type.value(),
+            self.params.nl_proc_order.value(),
+        );
+
+        self.proc_state = new_state;
+
+        self.non_linear_processors
+            .iter_mut()
+            .for_each(|x| *x = NonlinearProcessor::from_state(new_state));
+
+        self.peak_meter_decay_weight = 0.25f64
+            .powf((buffer_config.sample_rate as f64 * PEAK_METER_DECAY_MS / 1000.0).recip())
+            as f32;
+
         true
     }
 
     fn reset(&mut self) {
-        // Reset buffers and envelopes here. This can be called from the audio thread and may not
-        // allocate. You can remove this function if you do not need it.
-        self.first_order_nlprocs.iter_mut().for_each(|x| {
-            x.reset(self.params.nl_proc_type.value());
-        });
-        self.second_order_nlprocs.iter_mut().for_each(|x| {
-            x.reset(self.params.nl_proc_type.value());
-        });
+        let new_state = State(
+            self.params.nl_proc_type.value(),
+            self.params.nl_proc_order.value(),
+        );
+
+        self.proc_state = new_state;
+
+        self.non_linear_processors
+            .iter_mut()
+            .for_each(|x| *x = NonlinearProcessor::from_state(new_state));
 
         self.oversamplers.iter_mut().for_each(|x| x.reset());
         self.over_sample_process_buf
@@ -272,20 +273,26 @@ impl Plugin for NonlinearAdaa {
         _context: &mut impl ProcessContext<Self>,
     ) -> ProcessStatus {
         for (_, block) in buffer.iter_blocks(MAX_BLOCK_SIZE as usize) {
-            for (block_channel, (oversampler, (filter, (first_order_proc, second_order_proc)))) in
-                block.into_iter().zip(
-                    self.oversamplers.iter_mut().zip(
-                        self.pre_filters.iter_mut().zip(
-                            self.first_order_nlprocs
-                                .iter_mut()
-                                .zip(self.second_order_nlprocs.iter_mut()),
-                        ),
-                    ),
-                )
-            {
+            for (block_channel, oversampler, filter, nl_processor, in_meter, out_meter) in izip!(
+                block,
+                &mut self.oversamplers,
+                &mut self.pre_filters,
+                &mut self.non_linear_processors,
+                &mut self.input_meters,
+                &mut self.output_meters,
+            ) {
                 if self.params.os_level.value() != oversampler.get_oversample_factor() {
                     oversampler.set_oversample_factor(self.params.os_level.value());
                 };
+
+                let p_state = State(
+                    self.params.nl_proc_type.value(),
+                    self.params.nl_proc_order.value(),
+                );
+
+                self.proc_state = p_state;
+
+                nl_processor.compare_and_change_state(p_state);
 
                 // set cutoff
                 let param_pre_filter_cutoff: &Smoother<f32> =
@@ -298,52 +305,52 @@ impl Plugin for NonlinearAdaa {
 
                 oversampler.process_up(block_channel, &mut self.over_sample_process_buf);
 
-                let order = self.params.nl_proc_order.value();
-                let style = self.params.nl_proc_type.value();
+                let samples_to_take =
+                    2_u32.pow(oversampler.get_oversample_factor() as u32) as usize * MAX_BLOCK_SIZE;
 
-                first_order_proc.match_or_set_proc_style(&style);
-                second_order_proc.match_or_set_proc_style(&style);
+                let mut in_amplitude = 0.0;
+                let mut out_amplitude = 0.0;
 
-                if order != self.proc_order {
-                    self.proc_order = order;
-                }
+                self.over_sample_process_buf
+                    .iter_mut()
+                    .take(samples_to_take)
+                    .for_each(|sample| {
+                        let gain = self.params.gain.smoothed.next();
+                        let output = self.params.output.smoothed.next();
 
-                match self.proc_order {
-                    AntiderivativeOrder::First => {
-                        self.over_sample_process_buf
-                            .iter_mut()
-                            .take(
-                                2_u32.pow(oversampler.get_oversample_factor() as u32) as usize
-                                    * MAX_BLOCK_SIZE,
-                            )
-                            .for_each(|sample| {
-                                let gain = self.params.gain.smoothed.next();
-                                let output = self.params.output.smoothed.next();
-
-                                *sample *= gain;
-                                *sample = first_order_proc.next_adaa(sample);
-                                *sample *= output;
-                            });
-                    }
-                    AntiderivativeOrder::Second => {
-                        self.over_sample_process_buf
-                            .iter_mut()
-                            .take(
-                                2_u32.pow(oversampler.get_oversample_factor() as u32) as usize
-                                    * MAX_BLOCK_SIZE,
-                            )
-                            .for_each(|sample| {
-                                let gain = self.params.gain.smoothed.next();
-                                let output = self.params.output.smoothed.next();
-
-                                *sample *= gain;
-                                *sample = second_order_proc.next_adaa(sample);
-                                *sample *= output;
-                            });
-                    }
-                };
+                        *sample *= gain;
+                        in_amplitude += *sample;
+                        *sample = nl_processor.process(*sample);
+                        *sample *= output;
+                        out_amplitude += *sample;
+                    });
 
                 oversampler.process_down(&mut self.over_sample_process_buf, block_channel);
+
+                if self.params.editor_state.is_open() {
+                    in_amplitude = (in_amplitude / samples_to_take as f32).abs();
+                    out_amplitude = (out_amplitude / samples_to_take as f32).abs();
+
+                    let current_input_meter = in_meter.load(Ordering::Relaxed);
+                    let current_out_meter = out_meter.load(Ordering::Relaxed);
+
+                    let new_input_meter = if in_amplitude > current_input_meter {
+                        in_amplitude
+                    } else {
+                        current_input_meter * self.peak_meter_decay_weight
+                            + in_amplitude * (1.0 - self.peak_meter_decay_weight)
+                    };
+
+                    let new_output_meter = if out_amplitude > current_out_meter {
+                        out_amplitude
+                    } else {
+                        current_out_meter * self.peak_meter_decay_weight
+                            + out_amplitude * (1.0 - self.peak_meter_decay_weight)
+                    };
+
+                    in_meter.store(new_input_meter, Ordering::Relaxed);
+                    out_meter.store(new_output_meter, Ordering::Relaxed);
+                }
             }
         }
         ProcessStatus::Normal
